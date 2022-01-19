ae3525f5b73982070cc86a76efcf460df71d9b93
Alvaro Ramirez Segura <alvaro.ramirez.seg@gmail.com>
2022-01-18 23:21:32 -0600

    Set main as initial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(){
-    super();
-
-    this.state = {
-      name: "Alvaro"
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
+function App() {
+  return (
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -25,13 +15,11 @@ class App extends Component {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React {this.state.name}
+          Learn React
         </a>
-        <button onClick={ () => this.setState({name: "Alvaro Ramirez"}) }> Change Text</button>
       </header>
     </div>
-    );
-  }
+  );
 }
 
 export default App;

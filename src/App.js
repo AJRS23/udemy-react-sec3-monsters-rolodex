2c75c8ed6e275a5c7191edef7cb0c8fd9da5dd21
Alvaro Ramirez <alvaro.ramirez.seg@gmail.com>
2022-01-22 23:31:20 -0600

    sec3-34 Card List Component Props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+
+import {CardList} from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -20,11 +21,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      {
-        this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name} </h1>
-        ))
-      }
+        <CardList name="Alvaro">
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))
+        }
+        </CardList>
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './card-list.style.css';
+
+export const CardList = props => {
+  console.log(props)
+  console.log(props.children);
+  return (
+    <div className="card-list"> {props.children}</div>
+  )
+}

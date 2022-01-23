5be3579c45a399a672af3b11f2f58be5899a9b81
Alvaro Ramirez <alvaro.ramirez.seg@gmail.com>
2022-01-23 00:20:51 -0600

    sec3-36 Card Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Card } from "../card/card.component";
 import './card-list.style.css';
 
 export const CardList = props => {
-  console.log(props)
-  console.log(props.children);
   return (
-    <div className="card-list"> {props.children}</div>
+    <div className="card-list"> 
+    {
+      props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster} />
+        
+      ))
+    }
+    
+    </div>
   )
 }

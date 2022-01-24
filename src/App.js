34bd67407b6b4b2a8fa261c1deb90349b9843af0
Alvaro Ramirez <alvaro.ramirez.seg@gmail.com>
2022-01-24 16:08:15 -0600

    sec3-43 Search Box Component several props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import {CardList} from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -27,16 +28,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input 
-          type="search" 
-          placeholder='Search a monster' 
-          onChange={e => {
-            this.setState(
-              {searchField: e.target.value},
-              () => console.log(this.state)
-            )
-          }}>
-        </input>
+        <SearchBox
+          placeholder='Search monsters'
+          handleChange={ e => this.setState({searchField: e.target.value})}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );
